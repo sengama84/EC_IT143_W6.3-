60f9858ecd2d1ce5913989163a8547da71d5ea70
sengama84 <sengama@byupathway.edu>
2026-02-14 10:12:08 +0200

    Add initial SQL file for function exercise

---
+++ EC_IT143_6.3_fwf_s2_se.sql
@@ -0,0 +1,11 @@
+/* 
+EC_IT143_6.3_fwf_s2_se.sql
+Author: Sakul Engama
+Purpose: Fun with Functions – Step 2
+*/
+
+-- CURRENT THINKING:
+-- The first name appears before the first space in ContactName.
+
+-- NEXT STEP:
+-- Use LEFT() and CHARINDEX() to isolate the first name.
