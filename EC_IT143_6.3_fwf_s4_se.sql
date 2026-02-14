ba6c9c08c3ab41e36f1ea4b29367b4a6ba20749a
sengama84 <sengama@byupathway.edu>
2026-02-14 10:15:00 +0200

    Create EC_IT143_6.3_fwf_s4_se.sql file
    Add SQL script for Fun with Functions – Step 4.

---
+++ EC_IT143_6.3_fwf_s4_se.sql
@@ -0,0 +1,11 @@
+/* 
+EC_IT143_6.3_fwf_s4_se.sql
+Author: Sakul Engama
+Purpose: Fun with Functions – Step 4
+*/
+
+-- Resource used:
+-- https://www.w3schools.com/sql/sql_ref_charindex.asp
+
+-- CHARINDEX finds the position of the space.
+-- LEFT extracts text before that position.
