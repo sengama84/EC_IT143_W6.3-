31563f93621413b44577af86e569a010d66976a8
sengama84 <sengama@byupathway.edu>
2026-02-14 11:36:55 +0200

    Create EC_IT143_6.3_fwt_s2_se.sql

---
+++ EC_IT143_6.3_fwt_s2_se.sql
@@ -0,0 +1,8 @@
+/*
+EC_IT143_6.3_fwt_s2_se.sql
+Purpose: Step 2 – Begin creating an answer
+*/
+
+-- IDEA:
+-- Add a column to store the last modified date
+-- Use a trigger to update it automatically
