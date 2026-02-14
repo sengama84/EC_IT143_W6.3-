fd9c5337917e78e8166f985801ef1c4365ea85e1
sengama84 <sengama@byupathway.edu>
2026-02-14 11:37:42 +0200

    Add SQL file for research on AFTER UPDATE triggers

---
+++ EC_IT143_6.3_fwt_s3_se.sql
@@ -0,0 +1,8 @@
+/*
+EC_IT143_6.3_fwt_s3_se.sql
+Purpose: Step 3 – Research and test solution
+*/
+
+-- Research:
+-- AFTER UPDATE triggers can capture modification events
+-- GETDATE() returns current date and time
