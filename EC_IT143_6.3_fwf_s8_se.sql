73424de952d551414445b62d753227a2539b1d89
sengama84 <sengama@byupathway.edu>
2026-02-14 10:25:45 +0200

    Create EC_IT143_6.3_fwf_s8_se.sql

---
+++ EC_IT143_6.3_fwf_s8_se.sql
@@ -0,0 +1,11 @@
+/* 
+EC_IT143_6.3_fwf_s8_se.sql
+Author: Sakul Engama
+Purpose: Fun with Functions – Step 8
+*/
+
+-- NEXT QUESTION:
+-- How can I extract the LAST name from the ContactName column?
+
+-- PLAN:
+-- Use SUBSTRING() with CHARINDEX() to capture text after the space.
