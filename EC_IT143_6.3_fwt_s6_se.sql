023379c2b2e35cc758bc894e3018814b6c888f51
sengama84 <sengama@byupathway.edu>
2026-02-14 11:42:29 +0200

    Create EC_IT143_6.3_fwt_s6_se.sql

---
+++ EC_IT143_6.3_fwt_s6_se.sql
@@ -0,0 +1,8 @@
+/*
+EC_IT143_6.3_fwt_s6_se.sql
+Purpose: Step 6 – Ask next question
+*/
+
+-- NEXT QUESTION:
+-- How can I track who last modified a record?
+-- Answer: Use SUSER_NAME() in another trigger
